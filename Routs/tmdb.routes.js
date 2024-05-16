857a7b14dba14fd5652c7f5e682daed19688c084
ShamAl-ShikhOmark <shamalshikhomar@gmail.com>
2024-05-16 18:35:24 +0000

    update

---
+++ Routs/tmdb.routes.js
@@ -0,0 +1,15 @@
+'use strict'; 
+
+const {Router} = require("express")
+const router = Router()
+const {trendingHandler,serachHandler,popularHandler,topratedHandler} = require("../controller")
+
+router.get('/trending',trendingHandler)             //get request using axios "/trending"
+
+router.get('/search',serachHandler)              //get request using axios "/search"
+
+router.get('/popular',popularHandler)            //get request using axios "/popular"
+
+router.get('/toprated',topratedHandler)        //get request using axios "/toprated"
+
+module.exports = router
